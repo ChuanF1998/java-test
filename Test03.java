a752fdda3f47f71dad85c620d857a4b08e77e76f
ChuanF1998 <895952166@qq.com>
2019-11-04 14:55:47 +0800

    练习3

---
+++ Test03.java
@@ -0,0 +1,10 @@
+/* 已知圆的半径radius= 1.5，求其面积*/
+
+public class Test03
+{
+       public static void main(String[] args){
+             double radius=1.5;
+             double area=radius*radius*3.14;
+             System.out.println("圆的面积为 "+area);
+       }
+}
